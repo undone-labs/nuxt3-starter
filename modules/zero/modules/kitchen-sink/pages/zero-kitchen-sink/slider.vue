171557f632d11d59007451f249e534f3a75cdb55
tikagan <30419893+tikagan@users.noreply.github.com>
2023-10-18 15:48:33 -0400

    feat: adds zero slider to kitchen sink

---
+++ modules/zero/modules/kitchen-sink/pages/zero-kitchen-sink/slider.vue
@@ -0,0 +1,46 @@
+<template>
+  <main class="page">
+
+    <h1 class="page-heading">
+      Slider
+    </h1>
+
+    <ZeroSlider slider-id="demo-slider">
+      <template
+        v-for="(panel, i) in panels"
+        :key="`slide-${i}`">
+        <div
+          class="slider-panel">
+          <img
+            class="panel-image"
+            :src="panel.image" />
+          <p v-html="panel.title" />
+        </div>
+      </template>
+
+    </ZeroSlider>
+
+  </main>
+</template>
+
+<script setup>
+// ======================================================================= Setup
+definePageMeta({
+  layout: 'zero-layout'
+})
+
+// ======================================================================== Data
+const { data: content } = await useAsyncData('content', () => {
+  return queryContent({
+    where: {
+      _path: { $contains: '/zero/kitchen-sink/slider-sample' }
+    }
+  }).find()
+})
+
+const panels = ref(content.value[0].panels)
+</script>
+
+<style lang="scss" scoped>
+
+</style>
