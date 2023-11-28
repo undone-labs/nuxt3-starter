f27307519c7c9eac5b15cf5adeec74ae20cdac69
svvimming <benmunozj@gmail.com>
2023-11-28 00:09:45 -0500

    feat: find path in svg slot

--- components/clip-path.vue
+++ components/clip-path.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="container">
 
+    <slot name="svg-path"></slot>
+
     <svg class="svg-clip-path">
       <defs>
         <clipPath
+          v-if="path"
           id="clip-path-element"
           clipPathUnits="objectBoundingBox"
-          :transform="transform">
+          :transform="`scale(${1 / dimensions.w} ${1 / dimensions.h})`">
           <path
             fill-rule="evenodd"
             clip-rule="evenodd"
@@ -16,8 +19,12 @@
       </defs>
     </svg>
 
-    <div class="clip-wrapper">
-      <div class="clipped-content">
+    <div
+      class="clip-wrapper"
+      :style="wrapper">
+      <div
+        :class="['clipped-content', anchor]"
+        :style="{ width: `${dimensions.w}px`, height: `${dimensions.h}px`}">
         <slot name="background"></slot>
       </div>
     </div>
@@ -33,16 +40,40 @@ import { parseSVG, makeAbsolute } from 'svg-path-parser'
 
 // ======================================================================= Props
 const props = defineProps({
-  path: {
+  targetWidth: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  targetHeight: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  anchorType: {
     type: String,
-    required: true,
-    default: ''
+    required: false,
+    default: 'center'
+  },
+  anchorPosition: {
+    type: Number,
+    required: false,
+    default: 1
+  },
+  borderRadius: {
+    type: Number,
+    required: false,
+    default: 0
   }
 })
 
+const path = ref('')
+const clipPathElementId = ref(useUuid().v4())
+const instance = getCurrentInstance()
+
 // ==================================================================== Computed
 const parsed = computed(() => {
-  const commands = parseSVG(props.path)
+  const commands = parseSVG(path.value)
   makeAbsolute(commands)
   return commands
 })
@@ -80,12 +111,38 @@ const bounds = computed(() => {
   }
 })
 
-const transform = computed(() => {
+const svgDimensions = computed(() => {
   const rX = Math.abs(bounds.value.maxX - bounds.value.minX)
   const rY = Math.abs(bounds.value.maxY - bounds.value.minY)
-  return `scale(${1 / rX} ${1 / rY})`
+  return { rX, rY }
 })
 
+const anchor = computed(() => `${props.anchorType}-${props.anchorPosition}`)
+const wrapper = computed(() => props.borderRadius ? { borderRadius: `${props.borderRadius}px` } : null)
+const scaleX = computed(() => props.targetWidth ? (props.targetWidth / svgDimensions.value.rX) : 1)
+const scaleY = computed(() => props.targetHeight ? (props.targetHeight / svgDimensions.value.rY) : 1)
+const dimensions = computed(() => {
+  const dim = {
+    w: svgDimensions.value.rX * scaleX.value,
+    h: svgDimensions.value.rY * scaleY.value
+  }
+  return typeof dim.w === 'number' && typeof dim.h === 'number' ? dim : { w: 1, h: 1 }
+})
+
+onMounted(() => {
+  path.value = findPathElement()
+})
+
+const findPathElement = () => {
+  const el = instance.subTree.children[0].children[0]?.el
+  if (el) {
+    const pathEl = el.nodeName.toLowerCase() === 'path' ? el : el.querySelector('path')
+    if (pathEl) {
+      return pathEl.getAttribute('d')
+    }
+  }
+  return null
+}
 </script>
 
 <style lang="scss" scoped>
@@ -104,7 +161,7 @@ const transform = computed(() => {
   top: 0;
   left: 0;
   overflow: hidden;
-  border-radius: toRem(8);
+  // border-radius: toRem(8);
 }
 
 .svg-clip-path {
@@ -116,8 +173,44 @@ const transform = computed(() => {
 }
 
 .clipped-content {
-  width: 300px;
-  height: 100%;
+  // --clip-path-id: '#clip-path-element';
   clip-path: url(#clip-path-element);
+  position: absolute;
+  &.center-1 {
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  &.center-2 {
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  &.center-3 {
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  &.center-4 {
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  &.corner-1 {
+    left: 0;
+    top: 0;
+  }
+  &.corner-2 {
+    right: 0;
+    top: 0;
+  }
+  &.corner-3 {
+    right: 0;
+    bottom: 0;
+  }
+  &.corner-4 {
+    left: 0;
+    bottom: 0;
+  }
 }
 </style>
--- modules/zero/modules/kitchen-sink/pages/zero-kitchen-sink/curves-poc.vue
+++ modules/zero/modules/kitchen-sink/pages/zero-kitchen-sink/curves-poc.vue
@@ -9,7 +9,16 @@
       <div class="grid">
 
         <div class="col-4">
-          <ClipPath :path="path">
+          <ClipPath
+            :target-width="500"
+            :target-height="500"
+            anchor-type="corner"
+            :anchor-position="4"
+            :border-radius="8">
+
+            <template #svg-path>
+              <TestSvg />
+            </template>
 
             <template #background>
               <div class="image-wrapper">
@@ -39,7 +48,6 @@
 </template>
 
 <script setup>
-const path = ref('M0.5 8.91406C0.5 4.49579 4.08172 0.914063 8.5 0.914063L509.5 0.914063C513.918 0.914063 517.5 4.49578 517.5 8.91406V550.414C517.5 554.832 513.918 558.414 509.5 558.414H101.5C97.0817 558.414 93.5 554.832 93.5 550.414V375.426C93.5 343.666 81.874 313.006 60.8165 289.231L33.1835 258.033C12.126 234.258 0.5 203.597 0.5 171.838V8.91406Z')
 </script>
 
 <style lang="scss" scoped>
