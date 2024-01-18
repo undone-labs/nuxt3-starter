4de4221fba961a336f5b9cf311fb06616add34e8
tikagan <30419893+tikagan@users.noreply.github.com>
2024-01-18 10:51:10 -0500

    feat: toast animation works with variable size toasts

--- packages/zero-core/modules/toaster/components/zero-toast.vue
+++ packages/zero-core/modules/toaster/components/zero-toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['toast', { unpopping } ]">
+  <div :class="['toast', { unpopping } ]" :style="animationVariables">
 
     <slot name="toast" :toast="toast" />
 
@@ -12,6 +12,11 @@ const props = defineProps({
   toast: {
     type: Object,
     required: true
+  },
+  toasterPadding: {
+    type: String,
+    required: false,
+    default: '1rem'
   }
 })
 
@@ -20,6 +25,9 @@ const store = useZeroToasterStore()
 const config = useRuntimeConfig().public.toaster
 const unpopping = ref(false)
 const toastTimeout = props.toast.timeout || config.timeout || 5000
+const animationVariables = {
+  '--toaster-padding': props.toasterPadding
+}
 
 // ======================================================================= Hooks
 onMounted(() => {
@@ -44,21 +52,22 @@ const initializeToastUnpop = () => {
 
 <style lang="scss" scoped>
 @keyframes poppingToast {
-  from { top: -6rem; opacity: 0; }
+  from { top: calc( -100% - var(--toaster-padding)); opacity: 0; }
   to { top: 0; opacity: 1; }
 }
 
 @keyframes unpoppingToast {
   from { top: 0; opacity: 1; }
-  to { top: -6rem; opacity: 0; }
+  to { top: calc( -100% - var(--toaster-padding)); opacity: 0; }
 }
 
 // ///////////////////////////////////////////////////////////////////// General
 .toast {
+  --toaster-padding: 0rem;
   animation: poppingToast 250ms ease-in-out forwards;
   overflow: hidden;
   &.unpopping {
-    animation: unpoppingToast 150ms ease-in-out forwards;
+    animation: unpoppingToast 250ms ease-in-out forwards;
   }
 }
 </style>
