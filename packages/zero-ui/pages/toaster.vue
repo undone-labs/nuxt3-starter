dd668c8f2e69905ab8dffdd7d7b96e1006b4ba24
tikagan <30419893+tikagan@users.noreply.github.com>
2024-01-18 10:50:27 -0500

    feat: different length demo toast messages

--- packages/zero-ui/pages/toaster.vue
+++ packages/zero-ui/pages/toaster.vue
@@ -39,9 +39,16 @@ const toasterStore = useZeroToasterStore()
  */
 
 const createToast = () => {
+  const messages = [
+    'This is a toast message',
+    'This is a toast message: Nulla vel ipsum a massa auctor tristique. Donec ornare ex purus, ac rhoncus libero porttitor eget. Quisque ut mi vitae arcu egestas tempor et vitae velit.',
+    'This is a toast message: Aenean ex lectus, rhoncus sed rutrum elementum, interdum quis velit.',
+    'This is a toast message: Quisque gravida vel tortor sit amet eleifend. Fusce sodales vehicula diam, et congue augue pellentesque sit amet. Vestibulum pulvinar, sem a dapibus viverra, mi nibh aliquet mi, sit amet tempor quam ex quis ex. Morbi quis tincidunt arcu. Aenean turpis augue, laoreet nec mattis in, ornare eu nunc.'
+  ]
+  const messageIndex = Math.floor(Math.random() * (3 - 0 + 1) + 0 )
   toasterStore.addMessage({
     type: 'toast',
-    message: 'This is a toast message'
+    message: messages[messageIndex]
   })
 }
 </script>
