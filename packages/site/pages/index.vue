b50ca886964bc7a6da8f76fe57da3404bdb1bbb3
tikagan <30419893+tikagan@users.noreply.github.com>
2024-03-07 18:00:31 -0500

    chore: cleanup component test implementation

--- packages/site/pages/index.vue
+++ packages/site/pages/index.vue
@@ -11,15 +11,3 @@
     </div>
   </div>
 </template>
-
-<script setup>
-const { data: markdown } = await useAsyncData('remarkdown-parser', async () => {
-  const result = await queryContent({
-    where: {
-      _path: { $contains: 'markdown-sample' }
-    }
-  }).find()
-  return result[0]
-})
-
-</script>
