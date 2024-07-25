ca35b90bfff23d6b7361dea4e808f5b66741b25a
tikagan <30419893+tikagan@users.noreply.github.com>
2024-07-25 11:37:56 -0400

    feat: conditionally display zero logs

--- packages/site/modules/site.js
+++ packages/site/modules/site.js
@@ -23,11 +23,13 @@ const meta = {
 // /////////////////////////////////////////////////////////////////////// Setup
 // -----------------------------------------------------------------------------
 const setup = (_, nuxt) => {
+  const options = nuxt.options
   const hex1 = '#3e1c00'
   const hex2 = '#3e1c00'
   const hex3 = '#FFFFFF'
-  console.log('\n  ⚡️', `${Chalk.underline.hex(hex1).bold('load:package ')}${Chalk.bgHex(hex2).hex(hex3).bold(' site ')}\n`)
-  const options = nuxt.options
+  if (options.zero?.displayZeroLogs) {
+    console.log('\n  ⚡️', `${Chalk.underline.hex(hex1).bold('load:package ')}${Chalk.bgHex(hex2).hex(hex3).bold(' site ')}\n`)
+  }
   options.css = options.css.reverse()
 }
 
