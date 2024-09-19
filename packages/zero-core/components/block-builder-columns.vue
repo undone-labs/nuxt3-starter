5506764527a75ab673a2e4f6099e84676fe3a5f2
Ben Munoz <ben@undoneventures.com>
2024-09-19 16:17:25 -0400

    feat: block builder columns component docs

--- packages/zero-core/components/block-builder-columns.vue
+++ packages/zero-core/components/block-builder-columns.vue
@@ -23,8 +23,20 @@
 </template>
 
 <script setup>
+/**
+ * @description Generates the columns that make up a [Block Builder](/zero-core/components/block-builder) page section. Each column renders a list of child blocks. This component can call itself recursively, in which case the parent instance turns into the grid containing child columns.
+ */
 // ======================================================================= Setup
 defineProps({
+  /**
+   * An array of columns to render within a parent section's grid. Each column object consists of the following optional properties:
+   * @param {string} col See [Gridlex docs](https://gridlex.devlint.fr/) for how to format this value. If this value includes `grid`, the component will be triggered to recursively call itself.
+   * @param {string} data-push-left See [Gridlex docs](https://gridlex.devlint.fr/) for how to format and how this will affect the column.
+   * @param {string} data-push-right See [Gridlex docs](https://gridlex.devlint.fr/) for how to format and how this will affect the column.
+   * @param {[Object]} columns When recursively calling itself, this array of column objects will be passed to the child [Block Builder Columns](/zero-core/components/block-builder-columns) component.
+   * @param {[string]} classes An array of classes to add to the column's inner content wrapper.
+   * @param {[Object]} blocks An array of block objects. The properties of each block object are bound to Vue's dynamic component, `<component />`. Each block object must include an `is` property that tells the dynamic component what component to render. The `is` key is deleted from the object before props are bound to the component instance.
+   */
   columns: {
     type: Array,
     required: true
@@ -37,6 +49,9 @@ const allowedPushAttributes = ['data-push-left', 'data-push-right']
 // ===================================================================== Methods
 /**
  * @method parseColumns
+ * @desc - Parses and formats the columns array to be used by the component render template.
+ * @param {[Object]} columns An array of column objects.
+ * @returns {[Object]}
  */
 
 const parseColumns = columns => {
@@ -56,6 +71,9 @@ const parseColumns = columns => {
 
 /**
  * @method getColumnPushAttributes
+ * @desc - Searches a column for the presence of one or both `data-push-left` and/or `data-push-right` attributes and returns an object with just these two attributes to bind to the column element.
+ * @param {Object} column The column object to parse.
+ * @returns {Object}
  */
 
 const getColumnPushAttributes = column => {
@@ -70,6 +88,9 @@ const getColumnPushAttributes = column => {
 
 /**
  * @method getColumnClasses
+ * @desc - Joins the classes in the column's classes array into a single string to add to the class list of the inner `.column-content` div.
+ * @param {Object} column The column object to parse.
+ * @returns {string}
  */
 
 const getColumnClasses = column => {
@@ -79,6 +100,9 @@ const getColumnClasses = column => {
 
 /**
  * @method getBlocks
+ * @desc - Tests if there are blocks to render outside the grid columns.
+ * @param {[Object]} section The section to test for the existence of a blocks array.
+ * @returns {[Object]|boolean}
  */
 
 const getBlocks = column => {
@@ -88,6 +112,9 @@ const getBlocks = column => {
 
 /**
  * @method stripIs
+ * @desc - Removes the `is` property from the props objects which will be bound to dynamically rendered block components.
+ * @param {Object} block The block object who's key-value pairs will be bound to the component instance as props.
+ * @returns {Object}
  */
 
 const stripIs = block => {
--- packages/zero-core/components/block-builder.vue
+++ packages/zero-core/components/block-builder.vue
@@ -23,8 +23,19 @@
 </template>
 
 <script setup>
+/**
+ * @description A high level page-builder component that generates a series of page sections containing columns and blocks from a JSON content scaffold. Generates child columns for each section using the [Block Builder Columns](/zero-core/components/block-builder-columns) component.
+ */
 // ======================================================================= Setup
 defineProps({
+  /**
+   * An array of page sections. Each section object can have the following properties:
+   * @param {string} id The DOM element id of the section
+   * @param {[string]} grid An array of strings to append to the section grid class (see [Gridlex docs](https://gridlex.devlint.fr/) for possible options).
+   * @param {[Object]} columns An array of column objects passed as the `columns` prop to [Block Builder Columns](/zero-core/components/block-builder-columns).
+   * @param {[Object]} blocks An array of block objects. The properties of each block object are bound to Vue's dynamic component, `<component />`. Each block object must include an `is` property that tells the dynamic component what component to render. The `is` key is deleted from the object before props are bound to the component instance. These components will render outside of a column and thus will be free from the grid positioning.
+   * @param {boolean} hide If set to true will remove this section from the list of sections to render.
+   */
   sections: {
     type: Array,
     required: true
@@ -34,6 +45,9 @@ defineProps({
 // ===================================================================== Methods
 /**
  * @method notHidden
+ * @desc - Filters hidden sections from the list of sections to render.
+ * @param {[Object]} sections The array of sections passed to the Block Builder as a prop.
+ * @returns {[Object]}
  */
 
 const notHidden = sections => {
@@ -42,6 +56,9 @@ const notHidden = sections => {
 
 /**
  * @method getGridClasses
+ * @desc - Append gridlex classes to the grid. Returns a single string used by the Gridlex grid. See props above for more info.
+ * @param {[string]} grid - The array of classes to append.
+ * @returns {string}
  */
 
 const getGridClasses = grid => {
@@ -54,6 +71,9 @@ const getGridClasses = grid => {
 
 /**
  * @method getBlocks
+ * @desc - Tests if there are blocks to render outside the grid columns.
+ * @param {[Object]} section The section to test for the existence of a blocks array.
+ * @returns {[Object]|boolean}
  */
 
 const getBlocks = section => {
@@ -63,6 +83,9 @@ const getBlocks = section => {
 
 /**
  * @method stripIs
+ * @desc - Removes the `is` property from the props objects which will be bound to dynamically rendered block components.
+ * @param {Object} block The block object who's key-value pairs will be bound to the component instance as props.
+ * @returns {Object}
  */
 
 const stripIs = block => {
