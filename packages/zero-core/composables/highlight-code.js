fedcc14eea0ea9a6fe8c99a19d45364d2baed3aa
Ben Munoz <ben@undoneventures.com>
2024-09-27 14:09:13 -0400

    feat: zero uuid and highlight code composable docs

--- packages/zero-core/composables/highlight-code.js
+++ packages/zero-core/composables/highlight-code.js
@@ -1,7 +1,7 @@
 /**
- * @todo this composable should also support the passing in of a custom HLJS as
- * well as custom language imports/registration, both from outside of this
- * composable's defaults
+ * @module zeroHighlightCode
+ * @desc A convenience wrapper for adding syntax highlighting to code strings using [highlight.js](https://highlightjs.org/). See the method below which is the default export.
+ * **TODO:** This composable should also support the passing in of a custom HLJS as well as custom language imports/registration, both from outside of this composable's defaults.
  */
 // ///////////////////////////////////////////////////////////////////// Imports
 // -----------------------------------------------------------------------------
@@ -21,6 +21,10 @@ hljs.registerLanguage('curl', hljsCurl)
 // -----------------------------------------------------------------------------
 /**
  * @method zeroHighlightCode
+ * @desc Adds syntax highlight to code strings. Returns an object containing a `code` key, the value of which is the newly highlighted code, and a `language` key, providing the code language.
+ * @param {string} code The code to add syntax highlighting to.
+ * @param {string} language The language the code is written in. Currently, the only supported values are `javascript`, `js`, `json` && `curl` - See note above.
+ * @returns {Object}
  */
 
 export default (code, language) => {
--- packages/zero-core/composables/uuid.js
+++ packages/zero-core/composables/uuid.js
@@ -1,3 +1,17 @@
+/**
+ * @module zeroUuid
+ * @desc A convenience wrapper for generating universal unique identifiers using Uuid. Imports the following methods from Uuid and exposes them in the object returned by zeroUuid():
+ * - `NIL`
+ * - `parse`
+ * - `stringify`
+ * - `v1`
+ * - `v3`
+ * - `v4`
+ * - `v5`
+ * - `validate`
+ * - `version`
+ * See [Uuid](https://github.com/uuidjs/uuid) for documentation of each method. 
+ */
 // ///////////////////////////////////////////////////////////////////// Imports
 // -----------------------------------------------------------------------------
 import {
