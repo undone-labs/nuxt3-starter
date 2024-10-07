e5b40bcdd4054af2e47ce313ea166cead365c4cc
Ben Munoz <ben@undoneventures.com>
2024-10-07 10:23:42 -0400

    feat: accordion component and store docs

--- packages/docs/content/zero-core/modules/accordion/components.md
+++ packages/docs/content/zero-core/modules/accordion/components.md
@@ -119,15 +119,17 @@ Called when the button is clicked. This method subsequently calls the [toggleAll
 # Zero Accordion
 
 
+A component with child sections that can be toggled to expand and collapse. This component is relatively simple, consisting of a slot for multiple [zeroAccordionSection](/zero-core/modules/accordion/components#zero-accordion-section) components.
 
+In the `onBeforeMount` hook of this component, an instance tracking object is set in the accordion store using the store's [setAccordion](/zero-core/modules/accordion/store#setaccordion) method. When the component is mounted, if the `toggleOnLoad` and `multiple` props are set to true, the [toggleAllSections](/zero-core/modules/accordion/store#toggleallsections) method of the accordion store will be called for this accordion instance.
 
 ## Props
 
 | Prop | type | description | values |
 | ---- | ---- | ----------- | ------ |
-| `accordionId` | string |  |  |
-| `multiple`<span>(optional)</span> | boolean |  |  |
-| `toggleOnLoad`<span>(optional)</span> | boolean |  |  |
+| `accordionId` | string | A unique identifier for this accordion instance. This same prop is present in all children components and its value should match those of its children. |  |
+| `multiple`<span>(optional)</span> | boolean | A boolean indicating whether or not multiple accordion sections can be expanded at the same time. `true` if so, `false` indicates expanded section states are mutually exclusive. |  |
+| `toggleOnLoad`<span>(optional)</span> | boolean | A boolean indicating whether or not all accordion sections should be expanded upon mounting the component. **If `multiple` is set to false, this setting has no effect.** |  |
 
 ## Slots
 
@@ -136,6 +138,16 @@ Called when the button is clicked. This method subsequently calls the [toggleAll
 
 **name:** `default`  **scoped:** `false`
 
+
+[zeroAccordionSections](/zero-core/modules/accordion/components#zero-accordion-section) should be added to this slot
+
 ## Methods
 
 #### handleKeyboardNavigation()
+
+
+The keyboard event handler that is called on window `keydown` events. Used to navigate the accordion with the keyboard. It will call the accordion store's [toggleAllSections](/zero-core/modules/accordion/store#toggleallsections) method if the 'f' and meta keys are pressed.
+
+| param | type | description |
+| ----- | ---- | ----------- |
+| `e` | Object | The [KeyboardEvent](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent) object. |
--- packages/zero-core/modules/accordion/stores/use-zero-accordion-store.js
+++ packages/zero-core/modules/accordion/stores/use-zero-accordion-store.js
@@ -1,3 +1,7 @@
+/**
+ * @module useZeroAccordionStore
+ * @desc A store for tracking [accordion](/zero-core/modules/accordion/components#zero-accordion) instances and their expanded/collapsed [section](/zero-core/modules/accordion/components#zero-accordion-section) states.
+ */
 // ///////////////////////////////////////////////////////////////////// Imports
 // -----------------------------------------------------------------------------
 import { defineStore } from 'pinia'
@@ -7,10 +11,19 @@ import { defineStore } from 'pinia'
 export const useZeroAccordionStore = defineStore('zero-accordion', () => {
   // ===================================================================== state
   /**
-   * Expected accordion structure:
+   * @member {Object} accordions
+   * @kind ref
+   * @desc An object containing nested objects representing individual accordion instances. Each nested object exists at the root level of this object, has a key corresponding to its accordionId and should be of the following structure:
+   * ```js
    * {
-   *  sections: []
+   *   accordionId: string,
+   *   multiple: boolean,
+   *   id: string,
+   *   children: Array,
+   *   active: Array|boolean,
+   *   allSectionsOpen: boolean
    * }
+   * ```
    */
   const accordions = ref({})
 
@@ -18,6 +31,8 @@ export const useZeroAccordionStore = defineStore('zero-accordion', () => {
 
   /**
    * @method setAccordion
+   * @desc Adds an accordion instance tracking object to the [accordions object](/zero-core/modules/accordion/store#accordions) above.
+   * @param {Object} payload The tracking object. Should have the structure defined in the [accordions object](/zero-core/modules/accordion/store#accordions) above.
    */
   const setAccordion = payload => {
     accordions.value[payload.accordionId] = payload
@@ -25,6 +40,8 @@ export const useZeroAccordionStore = defineStore('zero-accordion', () => {
 
   /**
    * @method removeAccordion
+   * @desc Removes an accordion tracking object from the accordions state above.
+   * @param {string} accordionId The ID of the accordion to remove.
    */
   const removeAccordion = accordionId => {
    delete accordions.value[accordionId]
@@ -32,6 +49,9 @@ export const useZeroAccordionStore = defineStore('zero-accordion', () => {
 
   /**
    * @method setAccordionSection
+   * @desc Adds a section to be tracked to the accordion tracking object in the accordions state above. This method pushes the section ID to the `children` array in the tracking above.
+   * @param {string} accordionId The ID of the accordion to add the section to.
+   * @param {string} sectionId The ID of the section.
    */
   const setAccordionSection = (accordionId, sectionId) => {
     if (accordions.value[accordionId].children.includes(sectionId)) { return }
@@ -40,6 +60,9 @@ export const useZeroAccordionStore = defineStore('zero-accordion', () => {
 
   /**
    * @method toggleAccordionSection
+   * @desc Toggles the expanded/collapsed state of an accordion section. If the toggle results in all sections of the accordion in question being set to expanded, the `allSectionsOpen` boolean in the accordion tracking object will be set to `true`. Otherwise, it is set to `false`.
+   * @param {string} accordionId The ID of the accordion that the section belongs to.
+   * @param {string} sectionId The ID of the section whose expanded or collapsed state should be toggled.
    */
   const toggleAccordionSection = (accordionId, sectionId) => {
     const active = accordions.value[accordionId].active
@@ -64,6 +87,8 @@ export const useZeroAccordionStore = defineStore('zero-accordion', () => {
 
   /**
    * @method toggleAllSections
+   * @desc Sets all sections belonging to an accordion to their expanded states.
+   * @param {string} accordionId The ID of the accordion in question.
    */
   const toggleAllSections = accordionId => {
     if (!accordions.value[accordionId].multiple) {
