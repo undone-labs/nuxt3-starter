7b955d960a38334c33f325cc9afeb656173203a5
Ben Munoz <ben@undoneventures.com>
2024-10-07 14:41:06 -0400

    feat: toaster module docs

--- packages/zero-core/modules/toaster/components/zero-toast.vue
+++ packages/zero-core/modules/toaster/components/zero-toast.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="toast-container">
-
+    <!-- 
+      @slot The content of the toast window.
+        @binding toast Binds the 'toast' prop object to the slot.
+    -->
     <slot name="toast" :toast="toast" />
 
   </div>
 </template>
 
 <script setup>
+/**
+ * @description A 'toast' component; a small window with a message for the user that pops up like toast out of a toaster. As soon as the component is mounted, the [unToast]() method is called which will automatically clear the toast after a set time interval.
+ * Additionally, a watcher is set to watch the toast prop's `jingle` key. The jingle property is an integer value which represents the number of 'repeats' of the same message. If this value is greater than zero, the watcher will call the unToast method below and set a timeout to update the toast message jingle property to `0`. This functionality effectively extends the life of an existing toast message if that message is repeatedly added rather than creating multiple popup windows with the same message.
+ */
 // ======================================================================= Setup
 const props = defineProps({
+  /**
+   * The toast object for this component instance that is held in the [toaster store](/zero-core/modules/toaster/store).
+   */
   toast: {
     type: Object,
     required: true
@@ -24,6 +34,7 @@ const toastTimer = ref(null)
 // ===================================================================== Methods
 /**
  * @method unToast
+ * @desc Sets a timer with a setTimeout function. Once the timer interval has elapsed, removes the toast message from view using the toaster store's [removeMessage](/zero-core/modules/toaster/store#removemessage) method. The time interval can be set in the nuxt.config.js file under `zero.modules.toaster.timeout` or it is derived from the toast prop's `.timeout` key.
  */
 
 const unToast = () => {
--- packages/zero-core/modules/toaster/stores/use-zero-toaster-store.js
+++ packages/zero-core/modules/toaster/stores/use-zero-toaster-store.js
@@ -1,3 +1,7 @@
+/**
+ * @module useZeroToasterStore
+ * @description A store for toast message data tracking. Allows 'toaster' messages to be conveniently added from anywhere within an app.
+ */
 // ///////////////////////////////////////////////////////////////////// Imports
 // -----------------------------------------------------------------------------
 import { defineStore } from 'pinia'
@@ -9,12 +13,29 @@ export const useZeroToasterStore = defineStore('zero-toaster', () => {
   const config = useRuntimeConfig().public.toaster
 
   // ===================================================================== state
+  /**
+   * @member {Array} toasts
+   * @kind ref
+   * @desc An array of toast message objects containing message data and data about how the message should be displayed by the [toast and toaster components](/zero-core/modules/toaster/components). Each message object has the following structure:
+   * ```js
+   * {
+   *   text: string, // toast message
+   *   id: string, // automatically generated using the zeroUuid composable
+   *   jingle: number, // automatically added, see the toast component for more info
+   *   timeout: number, // how long the message should display in milliseconds
+   *   from: string // the direction the toast should appear from, 'top' or 'bottom'
+   * }
+   * ```
+   */
   const toasts = ref([])
 
   // =================================================================== actions
 
   /**
    * @method addMessage
+   * @desc Add a message to be displayed with a [toast component](/zero-core/modules/toaster/components#zero-toast).
+   * @param {Object} payload An object containing the message to display. The object should at least have the following text property but can additionally have any of the properties outlines above in [toasts](/zero-core/modules/toaster/store#toasts).
+   * @param {string} payload.text The message to display.
    */
 
   const addMessage = payload => {
@@ -37,6 +58,8 @@ export const useZeroToasterStore = defineStore('zero-toaster', () => {
 
   /**
    * @method removeMessage
+   * @desc Removes a toast message object from the [toasts](/zero-core/modules/toaster/store#toasts) array.
+   * @param {string} id The ID of the toast to remove.
    */
 
   const removeMessage = id => {
@@ -46,6 +69,9 @@ export const useZeroToasterStore = defineStore('zero-toaster', () => {
 
   /**
    * @method updateToast
+   * @desc Updates a toast message object from the [toasts](/zero-core/modules/toaster/store#toasts) array.
+   * @param {string} id The ID of the toast to update.
+   * @param {Object} payload An object with properties and values to assign to the toast object in question. See [toasts](/zero-core/modules/toaster/store#toasts) for possible key-value pairs.
    */
 
   const updateToast = (id, payload) => {
